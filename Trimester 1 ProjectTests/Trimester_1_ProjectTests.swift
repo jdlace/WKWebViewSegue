a74a2d48bed0613c771365ff2bf0528a05f0c251
jdlace <jdlace@icloud.com>
2015-11-18 13:12:09 -0500

    Initial Commit

---
+++ Trimester 1 ProjectTests/Trimester_1_ProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  Trimester_1_ProjectTests.swift
+//  Trimester 1 ProjectTests
+//
+//  Created by Jonathan Lace on 11/18/15.
+//  Copyright © 2015 SHP. All rights reserved.
+//
+
+import XCTest
+@testable import Trimester_1_Project
+
+class Trimester_1_ProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
